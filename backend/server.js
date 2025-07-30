2f3dfdf5406fd8879ffd2507edbe1731f53f42c6
ritvik <ritvikchandrikapure@gmail.com>
2025-07-30 18:52:43 +0530

    changes in code

--- backend/server.js
+++ backend/server.js
@@ -70,9 +70,12 @@ app.post("/login", async (req, res) => {
 
     res.status(200).json({ message: "Login successful!" });
 
-  } catch (error) {
-    res.status(500).json({ error: "Server error." });
+  }  catch (err) {
+  if (err.code === 11000) {
+    return res.status(400).json({ message: "Email already exists" });
   }
+  res.status(500).json({ message: "Signup failed", error: err.message });
+}
 });
 
 app.post('/admin/login', (req, res) => {
